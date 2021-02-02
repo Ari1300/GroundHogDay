118a229acd8420bd9c360cf29f1d6a24614ee3de
Ariana MacArthur <ariana.macarthur@nwktc.edu>
2021-02-02 09:35:03 -0700

    I'm Trying

--- app.js
+++ app.js
@@ -6,5 +6,10 @@ app.get("/", (req, resp)=>{
   resp.send("It worked");
 });
 
+app.get("/returnAll", (req, resp)=>{
+  
+  resp.send("All Submissions")
+});
+
 app.listen(1985)
 console.log("Listening on port 1985");
